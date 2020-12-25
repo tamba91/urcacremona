0497b1d3ff5ebdcdd493a594bc777a1dc5590540
tamba91 <tamba.1991@hotmail.it>
2020-12-26 00:03:59 +0100

    prova https

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    fetchWeatherData('http://api.openweathermap.org/data/2.5/weather?q=Cremona&units=metric&lang=it&appid=b734a115782cc2c4b428f09c5d0a6d63', manageResults);
+    fetchWeatherData('https://api.openweathermap.org/data/2.5/weather?q=Cremona&units=metric&lang=it&appid=b734a115782cc2c4b428f09c5d0a6d63', manageResults);
 }
 
 function fetchWeatherData(url, manageResults) {
@@ -10,7 +10,7 @@ function fetchWeatherData(url, manageResults) {
 
 function manageResults(wData) {
     var iconDiv = document.querySelector('.weather-icon')
-    iconDiv.innerHTML = `<img src='http://openweathermap.org/img/wn/${wData.weather[0].icon}@2x.png' class='u-max-full-width'>`
+    iconDiv.innerHTML = `<img src='https://openweathermap.org/img/wn/${wData.weather[0].icon}@2x.png' class='u-max-full-width'>`
     document.querySelector('.temperature-value').textContent = `${wData.main.temp.toFixed()}°C`
     document.querySelector('.weather-desc').textContent = wData.weather[0].description
 }
