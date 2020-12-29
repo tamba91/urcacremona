848d04d23c5f932bbb4f7ac4455bd58c20fe9c7a
tamba91 <tamba.1991@hotmail.it>
2020-12-29 20:23:36 +0100

    responsive navbar fixed

--- public/javascripts/aris.js
+++ public/javascripts/aris.js
@@ -1,36 +1,34 @@
-window.onload = function () {
-    tinymce.init({
-        selector: '#post-body',  // change this value according to your HTML
-        plugins: 'autolink link',
-    });
-    var newPostForm = document.querySelector('#new-post-form')
-    var deletePostForm = document.querySelector('#delete-post-form')
-    // 2 form
-    newPostForm.style.display = 'none'
+tinymce.init({
+    selector: '#post-body',  // change this value according to your HTML
+    plugins: 'autolink link',
+});
+var newPostForm = document.querySelector('#new-post-form')
+var deletePostForm = document.querySelector('#delete-post-form')
+// 2 form
+newPostForm.style.display = 'none'
+deletePostForm.style.display = 'none'
+
+document.querySelector('#new-post-button').addEventListener('click', function () {
+    if (newPostForm.style.display == 'none') {
+        newPostForm.style.display = 'block'
+    }
+    else {
+        newPostForm.style.display = 'none'
+    }
     deletePostForm.style.display = 'none'
+})
 
-    document.querySelector('#new-post-button').addEventListener('click', function () {
-        if(newPostForm.style.display == 'none'){
-            newPostForm.style.display = 'block'
-        }
-        else {
-            newPostForm.style.display = 'none'
-        }    
-        deletePostForm.style.display = 'none'
-    })
+document.querySelector('#delete-post-button').addEventListener('click', function () {
+    document.querySelector('#delete-post-form').style.display = 'block'
+    document.querySelector('#new-post-form').style.display = 'none'
+    var container = document.querySelector('#checkbox-container')
+    if (!container.hasChildNodes()) {
+        fetch('/areariservata/postsinfo')
+            .then(response => response.json())
+            .then(posts => displayResultsInCheckbox(posts, container))
 
-    document.querySelector('#delete-post-button').addEventListener('click', function () {
-        document.querySelector('#delete-post-form').style.display = 'block'
-        document.querySelector('#new-post-form').style.display = 'none'
-        var container = document.querySelector('#checkbox-container')
-        if (! container.hasChildNodes()) {
-            fetch('/areariservata/postsinfo')
-                .then(response => response.json())
-                .then(posts => displayResultsInCheckbox(posts, container))
-
-        }
-    })
-}
+    }
+})
 
 function displayResultsInCheckbox(items, divContainerId) {
 
