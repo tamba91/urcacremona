0c159d5d2d07cacfc744aaf647dc04f00d641494
tamba91 <tamba.1991@hotmail.it>
2020-12-29 21:23:48 +0100

    node-fetch installed and required

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var postController = require('../controllers/postController')
-
+var postController = require('../controllers/postController');
+var fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=Cremona&units=metric&lang=it&appid=b734a115782cc2c4b428f09c5d0a6d63')
+    .then(function(result){
+      result.json()
+        .then(function(data){
+          console.log(data)
+        })
+    })
   postController.getPosts()
     .then(function (posts) {
       res.render('index', { title: 'URCA sezione di Cremona', blogPosts: posts });
